6d9953a1d54cc5ff0fc46c40f0fee81df0b8972f
Leo Moraes <moraes.leo@gmail.com>
2020-05-05 20:54:43 +0000

    player nativo do chrome

--- src/pages/Pitch/index.js
+++ src/pages/Pitch/index.js
@@ -6,9 +6,11 @@ function Pitch(){
   return(
     <div>
       <Banner />
-      <h1>Pitch Page</h1>
-      
+      <h1>Link alternativo</h1>
+
       <div><a href="https://storyboarders.com/storyboard/jnx4oj4ebp-testeexport">Veja o Pitch</a></div>
+
+      <video controls="" autoplay="" name="media"><source src="https://digitalizeme.s3-sa-east-1.amazonaws.com/digitalizeme-pitch-investidor.mp4" type="video/mp4"></video>
     </div>
   )
 }
