4748a100400dd8a9140197751bbe7ee2ea41dbb5
Leo Moraes <moraes.leo@gmail.com>
2020-05-06 00:49:44 +0000

    cleaned

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,7 @@ import './styles.css';
 function Footer() {
     return (
         <footer>
-            <link href="https://admin.chatcompose.com/static/PT/global/css/main.1b3f9fb9.css" rel="stylesheet"/>    
-            <script async type="text/javascript" src="https://admin.chatcompose.com/static/PT/global/js/main.c9ea3138.js" user="leomoraes-digitalizeme" landing="true" lang="PT"></script> 
-            <div className="copy">
+             <div className="copy">
                 <p>© Digitalize.me 2020.</p>
                 <p>Todos os direitos reservados.</p>
             </div>
