1639b9a4f807efb8b4b1e229b4fcc7868f6ae6a9
Juan Carlo <juancarlo.h.junger@hotmail.com>
2020-05-05 23:09:17 -0300

    Update chat links and styles; Newslleter get email alert

--- src/components/ChatBot/index.js
+++ src/components/ChatBot/index.js
@@ -3,12 +3,16 @@ import React from 'react';
 import './styles.css';
 
 import { FaFacebookMessenger, FaWhatsapp } from 'react-icons/fa';
-import { BsChevronRight } from 'react-icons/bs';
+import { FiTerminal } from 'react-icons/fi';
 
 function ChatBot() {
 
     function wppLink() {
         return window.location.href = "whatsapp://send?phone=+14153048334&text=user=leomoraes-digitalizeme";
+    }
+
+    function fbLink() {
+        alert("Link em manutenção...");
     }
 
     return (
@@ -17,11 +21,11 @@ function ChatBot() {
                 <div className="content">
                     <div className="chat__card">
                         <div className="chat__card__form">
-                            <span><BsChevronRight size={16} color="#2d2d2d" /></span>
-                            <span><a href="https://admin.chatcompose.com/testbotland/leomoraes-digitalizeme/PT" target="chat">Olá eu sou a Ariela</a> ...</span>
+                            <span><FiTerminal size={16} color="#2d2d2d" /></span>
+                            <span>Olá eu sou a Ariela. Clique <a href="https://admin.chatcompose.com/testbotland/leomoraes-digitalizeme/PT" target="chat">aqui</a> e vamos conversar.</span>
                         </div>
                         <div className="chat__card__socials">
-                            <FaFacebookMessenger size={32} color="#ffffff" id="fb" />
+                            <FaFacebookMessenger size={32} color="#ffffff" id="fb" onClick={fbLink} />
                             <FaWhatsapp size={32} color="#ffffff" id="wpp" onClick={wppLink} />
                         </div>
                     </div>
--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
 function Newsletter() {
+    const [email, setEmail] = useState('');
+
+    function handleData(e) {
+        e.preventDefault();
+        alert(`Olá ${email}, não salvamos seu email ainda.`);
+    }
     return (
         <section className="newsletter">
             <div className="container">
                 <h1>Newsletter</h1>
-                <p>Insira seu email para receber as atualizações.</p>
-                <form>
-                    <input type="text" placeholder="Digite seu email ..." required />
+                <p>Insira seu email para receber as atualizações da Ariela.</p>
+                <p id="temporary">Por enquanto seu email não será salvo.</p>
+                <form onSubmit={handleData}>
+                    <input type="email" onChange={e => setEmail(e.target.value)} placeholder="Digite seu email ..." required />
                     <button type="submit">Enviar</button>
                 </form>
             </div>
