f7d3edadb79e0b569874e838c50153f3d3af0c87
Leo Moraes <moraes.leo@gmail.com>
2020-05-06 00:36:13 +0000

    fix the code of launch

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,8 @@ import './styles.css';
 function Footer() {
     return (
         <footer>
-            <link href="https://admin.chatcompose.com/static/land/main.c2b8f225.css" rel="stylesheet"/>    
-            <script async type="text/javascript" src="https://admin.chatcompose.com/static/PT/global/export/js/main.c9ea3138.js" user="leomoraes-digitalizeme" landing="true" lang="PT"></script> 
+            <link href="https://admin.chatcompose.com/static/PT/global/css/main.1b3f9fb9.css" rel="stylesheet"/>    
+            <script async type="text/javascript" src="https://admin.chatcompose.com/static/PT/global/js/main.c9ea3138.js" user="leomoraes-digitalizeme" landing="true" lang="PT"></script> 
             <div className="copy">
                 <p>© Digitalize.me 2020.</p>
                 <p>Todos os direitos reservados.</p>
