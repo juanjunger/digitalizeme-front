388ea2cfc25dbedbe435addd83df64d0a15475a1
Leo Moraes <moraes.leo@gmail.com>
2020-05-06 00:27:54 +0000

    fix words video vs pitch

--- src/pages/Video/index.js
+++ src/pages/Video/index.js
@@ -11,14 +11,14 @@ function Video() {
             <section className="video">
                 <div className="container">
                     <div className="video__header">
-                        <h1>VÃ­deo</h1>
+                        <h1>Vídeo</h1>
                     </div>
                     <div className="video__content">
-                        <iframe title="video" src="https://digitalizeme.s3-sa-east-1.amazonaws.com/digitalizeme-video-investidor.mp4" allow="autoplay;" allowFullScreen></iframe>
+                        <iframe title="video" src="https://digitalizeme.s3-sa-east-1.amazonaws.com/digitalizeme-pitch-investidor.mp4" allow="autoplay;" allowFullScreen></iframe>
                     </div>
                     
                     <div className="video__info">
-                        <p>Veja o <span>video</span> clicando <a href="https://digitalizeme.s3-sa-east-1.amazonaws.com/video+Final+-+Digitalize-me.pdf" target="_blank" rel="noopener noreferrer">aqui!</a></p>
+                        <p>Veja o <span>video</span> clicando <a href="https://digitalizeme.s3-sa-east-1.amazonaws.com/pitch+Final+-+Digitalize-me.pdf" target="_blank" rel="noopener noreferrer">aqui!</a></p>
                     </div>
                 </div>
             </section>
