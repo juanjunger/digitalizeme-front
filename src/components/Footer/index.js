57bdba6b486dd71c9e384c31b933e4b6f3688aa9
Leo Moraes <moraes.leo@gmail.com>
2020-05-06 00:30:54 +0000

    loading chatbot script

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,10 @@ import './styles.css';
 function Footer() {
     return (
         <footer>
+            <link href="https://admin.chatcompose.com/static/land/main.c2b8f225.css" rel="stylesheet"/>    
+            <script async type="text/javascript" src="https://admin.chatcompose.com/static/PT/global/export/js/main.c9ea3138.js" user="leomoraes-digitalizeme" landing="true" lang="PT"></script> 
             <div className="copy">
-                <p>© Digitalizeme 2020.</p>
+                <p>© Digitalize.me 2020.</p>
                 <p>Todos os direitos reservados.</p>
             </div>
         </footer>
